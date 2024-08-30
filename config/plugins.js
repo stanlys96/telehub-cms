b4f1fa85eb94feebe3c29f368d57308feafaa1c7
stanlys96 <stanlyskwok@gmail.com>
2024-08-30 23:18:56 +0700

    wlao

--- config/plugins.js
+++ config/plugins.js
@@ -4,4 +4,8 @@ module.exports = () => ({
       defaultDepth: 5, // Default is 5
     },
   },
+  rest: {
+    defaultLimit: 100,
+    maxLimit: 1000, // Set this to your desired maximum limit
+  },
 });
